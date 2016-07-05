bee9611757cd8908c259e4ef1f62efeeeb902119
Wenceslao Paez <wcpaez@gmail.com>
2016-07-05 11:03:32 -0500

    Initial project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IdpRubyExample::Application.config.secret_token = 'fb87e10f1df0d7b25949ffd2874e020ce96f2e6365aa44b6d90590889a6c4ce1de8b5122ebffe90bc2121104275a49a093cf6eff47ce3913a446384a42770509'
